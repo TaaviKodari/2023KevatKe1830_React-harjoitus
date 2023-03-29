86ee5fa2306a55b12e4da82061a899d11f8e86fb
Your Name <you@example.com>
2023-03-29 19:30:56 +0300

    List remove kesken

--- 2023kevatke1830_react_harjoitus/src/pages/List.jsx
+++ 2023kevatke1830_react_harjoitus/src/pages/List.jsx
@@ -3,13 +3,40 @@ import { useState } from "react";
 
 export const List =()=>{
     const [content,setContent] = useState([]);
+    const [inputValue,setInputValue]= useState('');
+
+    function clearList(){
+        setContent([]);
+    }
+
     return(
         <div>
             <div>
-                <form>
-                    <input type="text" required/>
+                <form onSubmit={(event)=>{
+                    event.preventDefault()
+                    const temp = [inputValue,...content];
+                    setContent(temp);
+                    setInputValue('');
+                }}>
+                    <input type="text" 
+                     placeholder="Type here.."
+                     value={inputValue}
+                     onChange={e => setInputValue(e.target.value)}
+                     required/>
+
                     <input type="submit"value="Submit" />
+                    {
+                        content.map((item)=>(
+                            <ul>
+                                <li>
+                                    <h2>{item}</h2>
+                                    <button>Remove</button>
+                                </li>
+                            </ul>
+                        ))
+                    }
                 </form>
+                <button onClick={clearList}>Clear List</button>
             </div>
         </div>
     );
