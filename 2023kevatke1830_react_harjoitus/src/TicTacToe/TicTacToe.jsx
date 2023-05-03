85dd174160d862c2bba0ab018854010a201aaa71
Your Name <you@example.com>
2023-05-03 19:30:54 +0300

    checkwinner walmis

--- 2023kevatke1830_react_harjoitus/src/TicTacToe/TicTacToe.jsx
+++ 2023kevatke1830_react_harjoitus/src/TicTacToe/TicTacToe.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import './TicTacToe.css';
 import { Board } from "./Board";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const intialBoard=['','','','','','','','',''];
 
 export const TicTacToe = ()=>{
     const[gameState, setGameState] = useState(intialBoard);
     const[isXTurn, setIsXTurn] = useState(true);
+    const[status, setStatus] = useState('');
+
+    useEffect(()=>{
+        const winner = checkWinner();
+        if(winner)
+        {
+            setStatus(`Winner: ${winner}`);
+        }
+    },[gameState])
 
     const onSquareClick = (index) =>{
         let strings = Array.from(gameState);
@@ -20,10 +30,32 @@ export const TicTacToe = ()=>{
         setIsXTurn(!isXTurn);
     }
 
+    const checkWinner = () =>{
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ];
+        for (let i = 0; i < lines.length; i++){
+            const[a,b,c] = lines[i];
+            if(gameState[a] && gameState[a] === gameState[b] 
+                && gameState[a] === gameState[c]){
+                    return gameState[a];
+                }
+        }
+        return null;
+    }
+
     return(
         <div>
             <h1>Tic-Tac-Toe</h1>
             <Board gameState={gameState} onSquareClick={onSquareClick}/>
+            {status}
         </div>
     );
 }
